4e4bba6ecf48dcaf3746795f312c6f915f3a9ec2
Bartosz Maciejewski <bkmac511@gmail.com>
2024-01-27 18:11:09 +0100

    divide lambda function & tests

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    float meters = 3.6;
+    int kmh;
+
+    auto divide = [](int kmh, int ms) -> int {
+        return kmh / ms;
+    };
+
+    cout << "insert km/h: " << endl;
+    cin >> kmh;
+
+    int result = divide(kmh,meters);
+
+    cout << result << endl;
+
     return 0;
 }
