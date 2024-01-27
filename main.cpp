090d2299c52b10097a8d019ee57a40a656e9e7bb
Bartosz Maciejewski <bkmac511@gmail.com>
2024-01-27 22:20:05 +0100

    now conversion process can be reversed ✅

--- main.cpp
+++ main.cpp
@@ -4,18 +4,38 @@ using namespace std;
 
 int main() {
 
-    float meters = 3.6;
+    // km/h to m/s
+    float meters_per_second = 3.6;
     float kmh;
 
-    auto divide = [meters](int kmh) -> float {
-        return static_cast<float>(kmh) / meters;
+    auto divide = [meters_per_second](float kmh) -> float {
+        return kmh / meters_per_second;
     };
 
-    cout << "insert km/h: " << endl;
+    cout << "Insert km/h: " << endl;
     cin >> kmh;
 
-    float result = divide(kmh);
-    cout << result << endl;
+    if (kmh == 0) {
+        cout << "Insert a non-zero number to continue" << endl;
+        cin >> kmh;
+    }
+
+    float result_st = divide(kmh);
+    cout << "Result (km/h to m/s): " << result_st << endl;
+
+    // m/s to km/h
+    float meters_per_second_input;
+
+    auto multiply = [meters_per_second](float meters) -> float {
+        return meters * meters_per_second;
+    };
+
+    cout << "Insert m/s: " << endl;
+    cin >> meters_per_second_input;
+
+    float result_nd = multiply(meters_per_second_input);
+    cout << "Result (m/s to km/h): " << result_nd << endl;
 
     return 0;
 }
+
